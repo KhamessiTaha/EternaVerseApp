7f66b02ae9724512ab93738238fc2a8c13b2dab7
Taha Khamessi <taha.khamessi@gmail.com>
2025-02-08 17:59:20 +0100

    user routes+authMiddleware+UserProtectedRoute

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
 
 
 dotenv.config();
@@ -12,6 +14,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("EternaVerseApp API is running...");
