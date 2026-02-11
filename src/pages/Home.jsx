7495fe8f825c56a6138623f1110094356fec1fdd
KhamessiTaha <taha.khamessi@gmail.com>
2026-02-11 09:47:18 +0100

    EternaVerseApp instead of EternaVerse in the Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -108,7 +108,7 @@ const Home = () => {
           >
             Welcome to{" "}
             <span className="bg-gradient-to-r from-blue-400 via-purple-400 to-pink-400 bg-clip-text text-transparent animate-pulse">
-              EternaVerse
+              EternaVerseApp
             </span>
           </motion.h1>
 
